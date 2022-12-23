f9bb3a66b1a81490f99acb852e8ac8d55d453c2b
Reza Baharani <reza@foresightcares.com>
2022-12-23 15:42:12 -0500

    Removing redundant files.

--- ios/FSC_FrameProcessor/FSC_FrameProcessor.m
+++
@@ -1,4 +0,0 @@
-#import <VisionCamera/FrameProcessorPlugin.h>
-
-@interface VISION_EXPORT_SWIFT_FRAME_PROCESSOR(frame_processor, FSC_FrameProcessorPlugin)
-@end
--- ios/FSC_FrameProcessor/FSC_FrameProcessor.swift
+++
@@ -1,9 +0,0 @@
-@objc(FSC_FrameProcessorPlugin)
-public class FSC_FrameProcessorPlugin: NSObject, FrameProcessorPluginBase {
-  @objc public static func callback(_ frame: Frame!, withArgs _: [Any]!) -> Any! {
-    let buffer = frame.buffer
-    let orientation = frame.orientation
-    // code goes here
-    return []
-  }
-}
